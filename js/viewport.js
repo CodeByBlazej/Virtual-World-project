61fb6c501bd3e663868853465400a3f196b133b9
CodeByBlazej <codebyblazej@gmail.com>
2024-02-29 21:30:13 +0000

    add dragging option to an editor

--- js/viewport.js
+++ js/viewport.js
@@ -4,6 +4,14 @@ class Viewport {
     this.ctx = canvas.getContext("2d");
 
     this.zoom = 1;
+    this.offset = new Point(0, 0);
+
+    this.drag = {
+      start: new Point(0, 0),
+      end: new Point(0, 0),
+      offset: new Point(0, 0),
+      active: false
+    }
 
     this.#addEventListeners();
   }
@@ -17,6 +25,23 @@ class Viewport {
 
   #addEventListeners() {
     this.canvas.addEventListener("mousewheel", this.#handleMouseWheel.bind(this));
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+    this.canvas.addEventListener("mouseup", this.#handleMouseUp.bind(this));
+  }
+
+  #handleMouseDown(evt) {
+    if(evt.button == 1) { // middle button
+      this.drag.start = this.getMouse(evt);
+      this.drag.active = true;
+    }
+  }
+
+  #handleMouseMove(evt) {
+    if (this.drag.active) {
+      this.drag.end = this.getMouse(evt);
+      this.drag.offset = subtract(this.drag.end, this.drag.start);
+    }
   }
 
   #handleMouseWheel(evt) {
