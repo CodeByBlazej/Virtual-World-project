bdab04de3ba0a5426a72a7357bd43a6338523864
CodeByBlazej <codebyblazej@gmail.com>
2024-03-15 22:47:02 +0000

    transform polygon into trapezoid
    Trapezoid coming out of 1 point. Next will be created on the opposite point to finally get connected.

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -11,12 +11,14 @@ class Envelope {
     const alpha = angle(subtract(p1, p2));
     const alpha_cw = alpha + Math.PI / 2;
     const alpha_ccw = alpha - Math.PI / 2;
-    const p1_ccw = translate(p1, alpha_ccw, radius);
-    const p2_ccw = translate(p2, alpha_ccw, radius);
-    const p2_cw = translate(p2, alpha_cw, radius);
-    const p1_cw = translate(p1, alpha_cw, radius);
+    
+    const points = [];
+    const step = Math.PI / 3;
+    for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+      points.push(translate(p1, i, radius));
+    }
 
-    return new Polygon([p1_ccw, p2_ccw, p2_cw, p1_cw]);
+    return new Polygon(points);
   }
 
   draw(ctx) {
