f83284a407b124e3d44f599662e6afa1e804929e
CodeByBlazej <codebyblazej@gmail.com>
2024-03-24 21:03:10 +0000

    create round shape connecting 2 points

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,10 +1,10 @@
 class Envelope {
-  constructor(skeleton, width) {
+  constructor(skeleton, width, roundness = 1) {
     this.skeleton = skeleton;
-    this.poly = this.#generatePolygon(width);
+    this.poly = this.#generatePolygon(width, roundness);
   }
 
-  #generatePolygon(width) {
+  #generatePolygon(width, roundness) {
     const { p1, p2 } = this.skeleton;
 
     const radius = width / 2;
@@ -13,11 +13,14 @@ class Envelope {
     const alpha_ccw = alpha - Math.PI / 2;
     
     const points = [];
-    const step = Math.PI / 10;
-    for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+    const step = Math.PI / Math.max(1, roundness);
+    const eps = step / 2;
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
       points.push(translate(p1, i, radius));
     }
-
+    for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
+      points.push(translate(p2, Math.PI +i, radius));
+    }
     return new Polygon(points);
   }
 
