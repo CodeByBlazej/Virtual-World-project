8b279bd66f701accdbc645501406d12d417bc7ec
CodeByBlazej <codebyblazej@gmail.com>
2024-04-09 21:52:15 +0100

    remove red points on intersections

--- js/primitives/polygon.js
+++ js/primitives/polygon.js
@@ -12,7 +12,6 @@ class Polygon {
   static break(poly1, poly2) {
     const segs1 = poly1.segments;
     const segs2 = poly2.segments;
-    const intersections = [];
     for(let i = 0; i < segs1.length; i++) {
       for(let j = 0; j < segs2.length; j++) {
         const int = getIntersection(
@@ -21,7 +20,6 @@ class Polygon {
 
         if(int && int.offset != 1 && int.offset != 0) {
           const point = new Point(int.x, int.y);
-          intersections.push(point);
           let aux = segs1[i].p2;
           segs1[i].p2 = point;
           segs1.splice(i + 1, 0, new Segment(point, aux));
@@ -31,7 +29,6 @@ class Polygon {
         }
       }
     }
-    return intersections;
   }
 
   drawSegments(ctx) {
