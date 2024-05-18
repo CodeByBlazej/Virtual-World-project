b3c630838f33ca7efda975b8279dcdd9c5ff9831
CodeByBlazej <codebyblazej@gmail.com>
2024-05-18 20:16:53 +0100

    add house bases along the roads

--- js/world.js
+++ js/world.js
@@ -75,7 +75,12 @@ class World {
       }
     }
 
-    return supports;
+    const bases = [];
+    for (const seg of supports) {
+      bases.push(new Envelope(seg, this.buildingWidth).poly);
+    }
+
+    return bases;
   }
 
   draw(ctx) {
