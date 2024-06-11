7a911ef5f043d6022d8bd7167369a10862170a83
CodeByBlazej <codebyblazej@gmail.com>
2024-06-11 21:17:21 +0100

    make trees depending on size of the project

--- js/world.js
+++ js/world.js
@@ -36,7 +36,7 @@ class World {
     this.trees = this.#generateTrees();
   }
 
-  #generateTrees(count = 10) {
+  #generateTrees() {
     const points = [
       ...this.roadBorders.map((s) => [s.p1, s.p2]).flat(),
       ...this.buildings.map((b) => b.points).flat()
@@ -52,7 +52,8 @@ class World {
     ];
 
     const trees = [];
-    while (trees.length < count) {
+    let tryCount = 0;
+    while (tryCount < 100) {
       const p = new Point(
         lerp(left, right, Math.random()),
         lerp(bottom, top, Math.random())
@@ -77,7 +78,9 @@ class World {
 
       if(keep) {
         trees.push(p);
+        tryCount = 0;
       }
+      tryCount++
     }
     return trees;
   }
